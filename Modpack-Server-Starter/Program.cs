bc94917a5d848fdc411c68361c0333c1ff20d1cf
WorlD <michaelgmx1@googlemail.com>
2021-04-01 00:22:13 +0200

    Initial commit

---
+++ Modpack-Server-Starter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modpack_Server_Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
